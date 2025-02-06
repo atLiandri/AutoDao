bf42aeb35385874ee8635837f885dd2c53c5e865
Sergei Nabatov <serynabatov@yandex.ru>
2025-02-06 22:51:40 +0100

    create proposal

--- agent/main.py
+++ agent/main.py
@@ -11,6 +11,10 @@ from langgraph.prebuilt import create_react_agent
 from cdp_langchain.agent_toolkits import CdpToolkit
 from cdp_langchain.utils import CdpAgentkitWrapper
 from langchain.tools import BaseTool
+from solcx import compile_standard, install_solc
+import json
+from web3 import Web3
+
 
 # Load environment variables
 load_dotenv()
@@ -205,6 +209,59 @@ async def root():
     """Root endpoint that confirms the API is running."""
     return {"status": "CDP Agent API is running"}
 
+@app.post("/create_proposal")
+async def create_proposal(request: ChatResponse):
+
+    with open("../contracts/DAO.sol", "r") as file:
+        proposal_contract = file.read()
+
+    compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"DAO.sol": {"content": proposal_contract}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"] # output needed to interact with and deploy contract 
+                }
+            }
+        },
+    },
+    solc_version="0.8.19")
+
+    # get bytecode
+    bytecode = compiled_sol["contracts"]["DAO.sol"]["SimpleDAO"]["evm"]["bytecode"]["object"]
+    # get abi
+    abi = json.loads(compiled_sol["contracts"]["DAO.sol"]["SimpleDAO"]["metadata"])["output"]["abi"]
+
+    # For connecting to ganache
+    w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+    chain_id = 1337
+    address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+    private_key = "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d"
+    # Create the contract in Python
+    DAO = w3.eth.contract(abi=abi, bytecode=bytecode)
+    # Get the number of latest transaction
+    nonce = w3.eth.get_transaction_count(address)
+
+    # build transaction
+    transaction = DAO.constructor().build_transaction({
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": address,
+        "nonce": nonce,
+    })
+    # Sign the transaction
+    sign_transaction = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+    # Send the transaction
+    transaction_hash = w3.eth.send_raw_transaction(sign_transaction.raw_transaction)
+    
+    # Wait for the transaction to be mined, and get the transaction receipt
+    transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
+
+
 if __name__ == "__main__":
     import uvicorn
+    install_solc("0.8.19")
     uvicorn.run(app, host="0.0.0.0", port=8000)
